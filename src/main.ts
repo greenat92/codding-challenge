b4b9704276e523678bedc1125e63da5864559bb7
Lakhdar Ben Zahia <lakhdar.benzahia@healy.world>
2022-08-27 11:11:34 +0200

    Added security packages like helmet and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
+import helmet from 'helmet';
+// import csurf from 'csurf';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // to protect the app from some well-known web vulnerabilities by setting HTTP headers appropriately
+   app.use(helmet());
+
+   // Cross-origin resource sharing (CORS) is a mechanism that allows resources to be requested from another domain
+   app.enableCors();
+
+   // csurf config 
+   // TODO: Solve csurf issue 
+  // app.use(csurf());
+ 
   
   // swagger config
   const document = createDocument(app);
