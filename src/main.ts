e1ea964fcf246ec46e72a068ea1c66a2286d787a
Lakhdar Ben Zahia <lakhdar.benzahia@healy.world>
2022-08-27 11:16:40 +0200

    added validation pipeline module

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { createDocument } from './swagger/swagger';
+import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
 
+
+const NEST_LOGGING = false;
 async function bootstrap() {
+  const opts: NestApplicationOptions = {};
+  if (!NEST_LOGGING) {
+    opts.logger = false;
+  }
   const app = await NestFactory.create(AppModule);
 
    // to protect the app from some well-known web vulnerabilities by setting HTTP headers appropriately
@@ -18,7 +25,8 @@ async function bootstrap() {
    // TODO: Solve csurf issue 
   // app.use(csurf());
  
-  
+  // validation pipeline
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   // swagger config
   const document = createDocument(app);
   SwaggerModule.setup('api/docs', app, document);
