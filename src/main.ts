f4931d11745cd98696d0fb7015cb5a6e85094f3a
Lakhdar Ben Zahia <lakhdar.benzahia@healy.world>
2022-08-27 11:02:38 +0200

    added swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // swagger config
+  const document = createDocument(app);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,12 @@
+import { SwaggerConfig } from './swagger.interface';
+
+/**
+ * Configuration for the swagger UI (found at /api).
+ */
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  title: 'Kollab codding challenge',
+  description:
+    'A service to fetch jobs',
+  version: '1.0',
+  tags: ['kollab challenge'],
+};
